8564ada7340084049ebb44263c358a556ef2fa43
sohamb17 <sohambelurgikar17@gmail.com>
2022-12-05 22:59:17 +0530

    Fill Timeline data

--- my-app/src/app/home/home.component.ts
+++ my-app/src/app/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+	jobs = [
+		{
+			company: 'ChatGen',
+			startDate: 'Mar 2020',
+			endDate: 'Apr 2021',
+			title: 'Frontend Developer',
+		},
+		{
+			company: 'Mattermost',
+			startDate: 'Aug 2020',
+			endDate: 'Dec 2020',
+			title: 'Contributor',
+		},
+	];
+	//   above can be input to timeline component from parent component
 }
--- my-app/src/app/timeline/timeline.component.ts
+++ my-app/src/app/timeline/timeline.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-timeline',
-  templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+	selector: 'app-timeline',
+	templateUrl: './timeline.component.html',
+	styleUrls: ['./timeline.component.css'],
 })
 export class TimelineComponent {
-
+	@Input() jobs: any[] = [];
 }
