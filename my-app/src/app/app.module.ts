228cc8f9e9cf59f8a1375c1cbfed7eff68169f88
sohamb17 <sohambelurgikar17@gmail.com>
2022-12-05 21:34:42 +0530

    Create timeline component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
